fa7a1c8727e82de0ada9a0c12162b44d05256e22
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-12-07 12:18:30 +0100

    Should do a search outwards from COM instead of searching to COM
    Its way too slow

--- day6/main.pl
+++ day6/main.pl
@@ -3,7 +3,8 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
-orbit(Center, Orbiter).
+orbit(Center, Orbiter) :-
+	string(Center), string(Orbiter).
 
 decode_orbit(Line, Orbit) :-
 	split_string(Line, ")", "", [Center, Orbiter]),
@@ -13,28 +14,53 @@ indirect_orbit(A,C) :-
 	orbit(A,B),
 	orbit(B,C).
 
+path(A,A,Orbits,0).   % two nodes are connected, if
 path(A,B,Orbits,Length) :-   % two nodes are connected, if
-	walk(A,B,[],Orbits), % - if we can walk from one to the other,
-	length(Edges, Length).
+	%format('Walking for orbiter: ~w~n', B),
+	walk(Orbits,A,B,[],[],Path), % - if we can walk from one to the other,
+	length(Path, Length).
 
-walk(A,B,Orbits,Orbits).
-walk(A,B,V,Orbits) :-
-  orbit(A,X) ,
-  not(member(X,V)) ,
-  (
-	B = X
-  ;
-	walk(X,B,[A|V])
-  ).
+walk(Orbits,A,B,Visited,AccPath,Out) :-
+	member(Member, Orbits),
+	Member = orbit(A,X),
+	Out = [Member|AccPath],
+	length(Out, Len),
+	%format('   found path~w\n', Len),
+	B = X.
+
+walk(Orbits,A,B,Visited,AccPath,Path) :-
+	length(AccPath, PathLength),
+	%format('path length ~w~n', [PathLength]),
+	member(Member, Orbits),
+	Member = orbit(A,X),
+	not(member(X,Visited)),
+	(
+		(
+			B = X
+		);
+		(
+			walk(Orbits,X,B,[A|Visited],[orbit(A,X)|AccPath],Path)
+		)
+	).
+
+sum(A, B, Out) :-
+	Out is A + B.
+
+sum_of_numbers(List, Out) :-
+	foldl(sum, List, 0, Out).
 
 indirect_orbits(A, B, Count, Num) :-
 	indirect_orbit(A,B).
 
 main(_) :-
-	read_lines_from_file('./input.txt', Lines),
+	read_lines_from_file('./testinput.txt', Lines),
 	maplist(decode_orbit, Lines, Orbits),
 	maplist([X,[A,B]]>>(X = orbit(A,B)), Orbits, Pairs),
 	flatten(Pairs, OrbitersWithDups),
 	sort(OrbitersWithDups, Orbiters),
-	path('Z58', 'COM', Orbits, Length),
-	write(Length).
+	%format('Orbits: ~w~n', [Orbits]),
+	maplist([Orbiter]>>path("COM", Orbiter, Orbits), Orbiters, Lengths),
+	%path("COM", "D", Orbits, Length),
+	format('Lengths: ~w~n', Lengths),
+	sum_of_numbers(Lengths, Result),
+	format('Total length: ~w~n', Result).
