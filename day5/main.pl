f7e98a3eba56379995303811590b1f3da7a4fc43
Kristian Hasselknippe <kristian@nortero.com>
2019-12-09 02:49:35 +0100

    :S

--- day5/main.pl
+++ day5/main.pl
@@ -1,4 +1,4 @@
-:- use_module(aoc).
+:- use_module(aoc5).
 :- use_module(library(apply)).
 :- use_module(library(lists)).
 :- use_module(library(dialect/hprolog)).
@@ -23,112 +23,103 @@ range(A,B,Out) :-
 
 % !decode_arg_modes(++ArgModeList:list<int>, ++NumArgs:int, -ArgModes:list<is_mode>).
 decode_arg_modes(ArgModeList, NumArgs, ArgModes) :-
-	%format('ArgModeList: ~w, NumArgs: ~w~n', [ArgModeList, NumArgs]),
 	NumArgsOneLess is NumArgs - 1,
 	range(0, NumArgsOneLess, Range),
-	maplist(
-		{ArgModeList, NumArgs}/[Index,Mode]>>(
-			(
-				nth0(Index, ArgModeList, ModeNum);
-				ModeNum = 0
-			),
-			decode_mode(ModeNum, Mode)
-		),
-		Range,
-		ArgModes
-	).
+	findall(Mode, (
+				member(Index, Range),
+				(nth0(Index, ArgModeList, Mode));
+				(Mode = 0)
+			), ArgModes).
 
-%! decode_intcode(+IntCode:int, -Opcode:opcode, -Arguments:list).
-decode_intcode(IntCode, Opcode, ArgModes) :-
+%! arg_modes(+IntCode:int, -Opcode:opcode, -Arguments:list).
+arg_modes(IntCode, Opcode, ArgModes) :-
 	number_digits(IntCode, Digits),
 	reverse(Digits,RevDigits),
 	(
-		[Oc, OpcodeMode |Arguments] = RevDigits;
-		([Oc] = RevDigits, Arguments = [])
+		([9,9|Arguments] = RevDigits, Opcode = 99);
+		([Opcode, _ |Arguments] = RevDigits);
+		([Opcode] = RevDigits, Arguments = [])
 	),
-	decode_mode(OpcodeMode, Mode),
-	decode_opcode(Oc, InstructionLen),
+	decode_opcode(Opcode, InstructionLen),
 	NumArgs is InstructionLen - 1,
 	length(ArgModes, NumArgs),
-	decode_arg_modes(Arguments, NumArgs, ArgModes),
-	Opcode = opcode(Oc, Mode).
+	decode_arg_modes(Arguments, NumArgs, ArgModes).
 
-get_arg(State, Arg, position, Out) :-
+get_arg(State, Arg, 0, Out) :-
 	nth0(Arg, State, Out).
-get_arg(State, Arg, immediate, Arg).
-
+get_arg(_, Arg, 1, Arg).
 set_arg(State, Pos, Arg, NewState) :-
-	%format('Set arg (position): ~w, (pos: ~w, arg: ~w), NS: ~w ~n', [State, Pos, Arg, NewState]),
+	format('       Set arg (position): ~w, (pos: ~w, arg: ~w), NS: ~w ~n', [State, Pos, Arg, NewState]),
 	replace_item_at_pos(State, Pos, Arg, NewState).
 
-apply_op(opcode(Op, OpMode), ArgModes, State, IP, NextState) :-
-	(
+apply_op(Op, ArgModes, State, IP, NextState) :-
+	((
 		Op = 1,
 		OpImpl = [A,B,Out]>>(Out is A + B)
 	);
 	(
 		Op = 2,
 		OpImpl = [A,B,Out]>>(Out is A * B)
-	),
+	)),
 	[AMode,BMode|_] = ArgModes,
-	split_at(IP, State, _, [H,A,B,Res|_]),
+	split_at(IP, State, _, [_,A,B,Res|_]),
 	get_arg(State, A, AMode, AVal),
+	format('foo: ~w~n', [AVal]),
 	get_arg(State, B, BMode, BVal),
-	ResVal is AVal + BVal,
-	set_arg(State, Res, ResVal, NextState).
+	call(OpImpl, AVal, BVal, ResVal),
+	format('NS ~w~n', [NextState]),
+	write(State),
+	format('   applying opcode: ~w with ~w and ~w =>(res:~w) res: ~w~n', [Op, A, B, Res, ResVal]),
+	set_arg(State, Res, ResVal, NextState),
+	format('        done~n').
 
-apply_op(opcode(3, OpMode), ArgModes, State, IP, NextState) :-
+apply_op(3, ArgModes, State, IP, NextState) :-
 	[InputMode|_] = ArgModes,
 	split_at(IP, State, _, [H,Res|_]),
 	read(UserInput),
 	%format('Res ~w, Input ~w, mode: ~w~n', [Res, Input, InputMode]),
 	set_arg(State, Res, UserInput, NextState).
 
-apply_op(opcode(4, OpMode), ArgModes, State, IP, NextState) :-
+apply_op(4, ArgModes, State, IP, NextState) :-
 	[InputMode] = ArgModes,
 	split_at(IP, State, _, [H,Res,Input|_]),
 	get_arg(State, Input, InputMode, InputVal),
 	format('OUTPUT: ~w~n', [InputVal]),nl,
 	set_arg(State, Res, InputVal, NextState).
 
-perform_operation_using_opcode(State, IP, opcode(OpVal,OpMode), ArgModes, NextState) :-
-	format('Opcode: (~w,~w), ArgModes: ~w~n', [OpVal, OpMode, ArgModes]),
+perform_operation_using_opcode(State, IP, OpVal, ArgModes, NextState) :-
+	format('Opcode: ~w, ArgModes: ~w~n', [OpVal, ArgModes]),
 	decode_opcode(OpVal, OpSize),
 	NumArgs is OpSize - 1,
-	%format('Applying opcode (op val: ~w, op mode: ~w)~n', [OpVal, OpMode]),
-	apply_op(opcode(OpVal, OpMode), ArgModes, State, IP, NextState).
+	format("123NS ~w~n", [NextState]),
+	format('  Applying opcode (op val: ~w, op size: ~w, argModes: ~w)~n', [OpVal, OpSize, ArgModes]),
+	apply_op(OpVal, ArgModes, State, IP, NextState).
 	%format('Next state: ~w~n', [NextState]).
 
 perform_operation(State, InstructionPointer, NextState, NextInstructionPointer) :-
 	format('Instruction pointer at: ~w~n', [InstructionPointer]),
 	nth0(InstructionPointer, State, Intcode),
-	%format('Int code: ~w~n', [Intcode]),
-	decode_intcode(Intcode, Opcode, ArgModes),
-	%format('Decoded opcode: ~w~n', [Opcode]),
-	opcode(OpcodeNum, _) = Opcode,
-	decode_opcode(OpcodeNum, InstructionLength),
+	format('Int code: ~w~n', [Intcode]),
+	arg_modes(Intcode, Opcode, ArgModes),
+	format('Decoded opcode: ~w for intcode: ~w~n', [ArgModes, Opcode]),
+	decode_opcode(Opcode, InstructionLength),
+	format('    opcode len: ~w~n', [InstructionLength]),
 	perform_operation_using_opcode(State, InstructionPointer, Opcode, ArgModes, NextState),
-	NextInstructionPointer is InstructionPointer + InstructionLength,
-	format('   next IP: ~w~n', [NextInstructionPointer]).
+	NextInstructionPointer is InstructionPointer + InstructionLength.
+	%format('   next IP: ~w~n', [NextInstructionPointer]).
 
-run_program_impl(State, IP, OutputState, Res) :-
+run_program_impl(State, IP, Res, Res) :-
+	nth0(IP, State, 99).
+run_program_impl(State, IP, Prev, Output) :-
 	nth0(IP, State, Inst),
-	format('Current state: ~w~n', [State]),
-	format('Next instruction pointer: ~w~n', [IP]),
-	format('Next instruction: ~w~n', [Inst]),
-	(
-		(
-			Inst = 99,
-			OutputState = Res
-		);
-		(
-			perform_operation(State, IP, NextState, NextInstructionPointer),
-			run_program_impl(NextState, NextInstructionPointer, NextState, Res)
-		)
-	).
+	%format('Current state: ~w~n', [State]),
+	%format('Next instruction pointer: ~w~n', [IP]),
+	%format('Next instruction: ~w~n', [Inst]),
+	perform_operation(State, IP, NextState, NextInstructionPointer),
+	run_program_impl(NextState, NextInstructionPointer, Prev, Output).
 
 run_program(State, OutputState) :-
-	run_program_impl(State, 0, OutputState).
+	run_program_impl(State, 0, State, OutputState).
 
 main(_) :-
 	read_lines_from_file("./input.txt", Lines),
@@ -136,24 +127,28 @@ main(_) :-
 	run_program(InitialState, OutputState),
 	format('Output state ~w~n', [OutputState]).
 
+test_perform_operation(State, IP, NIP, NS) :-
+	perform_operation(State, IP, NIP, NS),
+	format('NewState: ~w, NewIP: ~w~n', [NIP, NS]).
+
 :- begin_tests(opcode_tests).
 
 test(test_op_add_immediate) :-
-	perform_operation([1101, 5, 6, 3], 0, [1101, 5, 6, 11], 4).
+	test_perform_operation([1101, 5, 6, 3], 0, [1101, 5, 6, 11], 4).
 
 test(test_op_add_with_negative_immediate) :-
-	perform_operation([1101, 5, -6, 3], 0, [1101, 5, 6, -2], 4).
+	test_perform_operation([1101, 5, -6, 3], 0, [1101, 5, 6, -2], 4).
 
 test(test_op_add_position) :-
-	perform_operation([0001, 3, 3, 3], 0, [1,3,3,6], 4).
+	test_perform_operation([0001, 3, 3, 3], 0, [1,3,3,6], 4).
 
 test(test_op_mul_immediate) :-
-	perform_operation([1101, 5, 5, 3], 0, [1,3,3,25], 4).
+	test_perform_operation([1102, 5, 5, 3], 0, [1102,3,3,25], 4).
 
 test(test_op_mul_position) :-
-	perform_operation([1101, 3, 3, 3], 0, [1,3,3,9], 4).
+	test_perform_operation([1102, 3, 3, 3], 0, [1102,3,3,9], 4).
 
-test(test_op_3_immediate) :-
-	perform_operation([113, 3, 20, 3], 0, [113,3,20,20], 3).
+%test(test_op_3_immediate) :-
+%	test_perform_operation([113, 3, 20, 3], 0, [113,3,20,20], 3).
 
 :- end_tests(opcode_tests).
